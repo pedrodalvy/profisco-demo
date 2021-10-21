cbb18198902a91e2c10a5c155098357d04c07682
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-21 08:10:18 -0400

    add typeorm
    Adicionado Typeorm e arquivo de configuracao com banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormConfig from './config/orm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormConfig)],
   controllers: [],
   providers: [],
 })
---
+++ src/config/orm.config.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { env } from 'node:process';
+import * as path from 'node:path';
+
+dotenv.config();
+
+const ormConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: env.POSTGRES_HOST,
+  port: Number(env.POSTGRES_PORT),
+  username: env.POSTGRES_USER,
+  password: env.POSTGRES_PASSWORD,
+  database: env.POSTGRES_DB,
+  entities: [path.resolve(__dirname, '..', '**', '*.entity{.ts,.js}')],
+  migrations: [
+    path.resolve(__dirname, '..', 'database', 'migrations', '*{.ts,.js}'),
+  ],
+  cli: {
+    migrationsDir: path.resolve(__dirname, '..', 'database', 'migrations'),
+  },
+};
+
+module.exports = ormConfig;
